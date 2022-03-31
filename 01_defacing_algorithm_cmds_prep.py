42dccd7fb0b655e305cdf426a36e8fe43080f578
Arshitha <arsh2794@gmail.com>
2022-03-31 13:04:18 -0400

    directory restructuring

--- 01_defacing_algorithm_cmds_prep.py
+++ 01_defacing_algorithm_cmds_prep.py
@@ -1,7 +1,20 @@
+import argparse
 from os import fspath
 from pathlib import Path
 
-import constants
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Generate a swarm command file to deface T1w scans for a given BIDS dataset.')
+
+    parser.add_argument('-in', action='store', dest='input',
+                        help='Path to input BIDS dataset.')
+
+    parser.add_argument('-out', action='store', dest='output',
+                        help='Path to output dataset.')
+
+    args = parser.parse_args()
+    return Path(args.input), Path(args.output)
 
 
 def write_cmds_to_file(cmds_list, filepath):
@@ -14,26 +27,39 @@ def generate_cmds(output_dir, modality, scans_list):
     cmds_list = []
     for scan in scans_list:
         entities = scan.name.split('_')
-        subj_outdir = output_dir.joinpath(entities[0], entities[1], modality)
-        acq = entities[2].split('-')[1]
+        acq = [i.split('-')[1] for i in entities if i.startswith('acq-')]
+        if acq:
+            subj_outdir = output_dir.joinpath(entities[0], entities[1], modality, acq[0])
+        else:
+            subj_outdir = output_dir.joinpath(entities[0], entities[1], modality)
 
         # filename without the extensions
         prefix = scan.name.split('.')[0]
 
-        # make output directories within subject directory for afni_refacer, fsl_bet and fsl_anat outputs
-        fa_outdir = subj_outdir / 'fsl_anat' / acq
-        ar_outdir = subj_outdir / 'afni_refacer' / acq
-        fb_outdir = subj_outdir / 'fsl_bet' / acq
-        mkdir_cmds = f"mkdir -p {fa_outdir} {ar_outdir} {fb_outdir}"
+        # make output directories within subject directory for afni and fsl programs
+        fsl = subj_outdir / 'fsl'
+        afni = subj_outdir / 'afni'
+        mkdir_cmds = f"mkdir -p {fsl}/{{bet,fsl_deface}} {afni}/{{refacer,skullstrip}}"
 
-        # individual commands
-        fa_cmd = f"fsl_anat -o {fa_outdir} --nocleanup -i {scan}"
-        ar_cmd = f"@afni_refacer_run -input {scan} -mode_deface -no_clean -prefix {fspath(ar_outdir.joinpath(prefix))}"
-        fb_cmd = f"bet {scan} {fb_outdir.joinpath(prefix + '_bet.nii.gz')} -S -d"
+        # fsl commands
+        fsl_anat = f"fsl_anat -o {fsl.joinpath('fsl')} --nocleanup -i {scan}"
+        bet = f"bet {scan} {fsl.joinpath('bet', prefix + '_bet.nii.gz')} -S -d"
+
+        deface_prefix = fsl.joinpath('fsl_deface', prefix)
+        fsl_deface = f"fsl_deface {scan} {deface_prefix}_deface -d {deface_prefix}_defacing_mask " \
+                     f"-n {deface_prefix}_cropped_struc -m13 {deface_prefix}_orig_2_std -m12 {deface_prefix}_orig_2_cropped " \
+                     f"-m23 {deface_prefix}_cropped_2_std"
+
+        # afni commands
+        refacer = f"@afni_refacer_run -input {scan} -mode_deface -no_clean -prefix {fspath(afni.joinpath('refacer', prefix))}"
+
+        skullstrip_prefix = afni.joinpath('skullstrip', prefix + '_skullstrip')
+        skullstrip = f"3dSkullStrip -input {scan} -prefix {skullstrip_prefix}.nii.gz -push_to_edge; " \
+                     f"fslmaths {skullstrip_prefix}.nii.gz -bin {skullstrip_prefix}_binarized.nii.gz"
 
         full_cmd = '; '.join(
-            ["START=`date +%s`", mkdir_cmds, fa_cmd, ar_cmd, fb_cmd, "STOP=`date +%s`",
-             "RUNTIME=$((STOP-START))", "echo ${RUNTIME}"]) + '\n'
+            ["START=`date +%s`", mkdir_cmds, fsl_anat, bet, refacer, skullstrip, fsl_deface,
+             "STOP=`date +%s`", "RUNTIME=$((STOP-START))", "echo ${RUNTIME}"]) + '\n'
 
         cmds_list.append(full_cmd)
 
@@ -42,8 +68,10 @@ def generate_cmds(output_dir, modality, scans_list):
 
 def main():
     # generate bash commands and write to swarm file
-    defacing_cmds = generate_cmds(constants.OUTPUTS_DIR, 'anat', constants.T1W_SCANS)
-    write_cmds_to_file(defacing_cmds, 'new_algorithm_v2.swarm')
+    input, output = get_args()
+    t1_scans = [s for s in list(input.glob('sub-*/ses-*/anat/*run-*1*T1w.nii.gz'))]
+    defacing_cmds = generate_cmds(output, 'anat', t1_scans)
+    write_cmds_to_file(defacing_cmds, f'intermediate_commands_{input.parent.name}.swarm')
 
 
 if __name__ == "__main__":
--- constants.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-BIDS_DIR = Path('/data/BrainBlocks/joyce/bids')
-OUTPUTS_DIR = Path('/data/NIMH_scratch/hv_protocol/defacing_comparisons/new_algorithm_v2')
-REQ_ACQS = ['acq-mprage', 'acq-fspgr']
-T1W_SCANS = [s for s in list(BIDS_DIR.glob('sub-*/ses-*/anat/*run-01*T1w.nii.gz')) if s.name.split('_')[2] in REQ_ACQS]
