0b37ac23f1385807b2b4434727676fe7bbf7a8b8
Yaroslav Halchenko <debian@onerussian.com>
2024-05-07 14:55:04 -0400

    Perspective neurodocker invocation + produced Dockerfile (broken)
    It is based on WIP done in https://github.com/ReproNim/neurodocker/pull/611 to add support for Gentoo.
    But it is just a dirty draft so produced docker file is not good.

--- generate_container.sh
+++ generate_container.sh
@@ -6,26 +6,29 @@ generate() {
 	# more details might come on https://github.com/ReproNim/neurodocker/issues/330
 	[ "$1" == singularity ] && add_entry=' "$@"' || add_entry=''
 	#neurodocker generate "$1" \
-	ndversion=0.9.5
+	#ndversion=0.9.5
 	#ndversion=master
 	#docker run --rm repronim/neurodocker:$ndversion \
 	# ATM needs devel version  of neurodocker for a fix to AFNI recipe
+	#--base-image neurodebian:bookworm \
+	#--ndfreeze date=20240320 \
+	dest=/opt/dsst-defacing-pipeline
 	neurodocker \
 		generate "$1" \
-		--base-image neurodebian:bookworm \
-		--ndfreeze date=20240320 \
+		--pkg-manager portage \
+		--base-image "docker.io/gentoo/portage:20240324 as portage" \
+		--base-image "docker.io/gentoo/stage3:20240318" \
+		--gentoo gentoo_hash=2d25617a1d085316761b06c17a93ec972f172fc6 \
+		--install afni fsl \
 		--copy environment.yml /opt/environment.yml \
+		--copy src "$dest" \
 		--miniconda \
 			version=latest \
 			env_name=dsstdeface \
 			env_exists=false \
 			yaml_file=/opt/environment.yml \
-		--pkg-manager=apt \
-		--install vim wget strace time ncdu gnupg curl procps pigz less tree \
-		--run "apt-get update && apt-get -y dist-upgrade" \
-		--afni method=binaries version=latest \
 		--user=dsst \
-		--entrypoint "bash"
+		--entrypoint "$dest/run.py"
 		#--run "curl -sL https://deb.nodesource.com/setup_16.x | bash - " \
 		#--install nodejs npm \
 		#--run "npm install -g bids-validator@1.14.4" \
